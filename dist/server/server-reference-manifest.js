9b21d701b67dd7ed40c89711de8e664b318b5cee
MD Abdul Muhaimen <amuhaimenpro829@gmail.com>
2025-06-28 11:47:51 +0600

    changes

--- dist/server/server-reference-manifest.js
+++ dist/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"jq5ZXneQlBqhpyGYMMBKWXuiLggl3y4BW4zYlsZGiio=\"\n}"
---
+++ dist/static/chunks/app_page_tsx_fbdaf659._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_b1a238d0._.js",
+  "static/chunks/_f717d92d._.js",
+  "static/chunks/node_modules_swiper_b167c7bf._.css"
+],
+    source: "dynamic"
+});
