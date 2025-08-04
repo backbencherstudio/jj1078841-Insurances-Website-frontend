779450597e2476ed90760cf021f2f1263680aa21
Miraz Hossain <105309402+Miraz661@users.noreply.github.com>
2025-08-04 16:25:48 +0600

    Update admin dashboard.

--- dist/static/chunks/app_(client)_layout_tsx_d16bd306._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_fd57914e._.js",
-  "static/chunks/_3937dd73._.js"
-],
-    source: "dynamic"
-});
