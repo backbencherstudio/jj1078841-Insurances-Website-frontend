ed1d47f0f9fe144e54e9ff1ab66a3976e94243db
Miraz Hossain <105309402+Miraz661@users.noreply.github.com>
2025-08-07 17:45:30 +0600

    Updated user dashboard.

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,11 +1,12 @@
 
 import React from 'react'
 import Dashboard from './_components/Dashboard';
+import MyClaim from '@/app/_components/sections/MyClaim';
 
 
 
 export default function DashboardHome() {
   return (
-    <Dashboard />
+    <MyClaim />
   );
 }
